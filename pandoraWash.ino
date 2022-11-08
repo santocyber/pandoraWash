47ca6ea31d7f6b26b57067f6e8c4bca7c42765ec
SantoCyber <santocyber@gmail.com>
2022-11-08 08:38:30 -0300

    Update pandoraWash.ino

--- pandoraWash.ino
+++ pandoraWash.ino
@@ -3,48 +3,28 @@
   Complete project details at https://github.com/santocyber/pandoraWash
   
 */
-
-#include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>   
-#include <ArduinoJson.h>
-#include <SPI.h>
 #include <WiFi.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Wire.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI();  
+
+// Wifi network station credentials
+#define WIFI_SSID "InternetSA"
+#define WIFI_PASSWORD "cadebabaca"
+// Telegram BOT Token (Get from Botfather)
+#define BOTtoken "5436108629:AAGxKKaYRn5JDtmyOLR8mhCDQeY59HgWQcc"
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+String id, text, welcome, from_name;//Váriaveis para armazenamento do ID e TEXTO gerado pelo Usuario
+unsigned long tempo;
 
 
-//Configura fuso horario em segundos
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
-
-#define NTP_OFFSET  19800 // In seconds
-#define NTP_INTERVAL 60 * 1000    // In miliseconds
-#define NTP_ADDRESS  "pool.ntp.org"
-
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
-
-//define saidas do display
-#define OLED_MOSI  23
-#define OLED_CLK   18
-#define OLED_DC    4
-#define OLED_CS    5
-#define OLED_RESET 2
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-
-
-
-// Replace with your network credentials
-const char* ssid = "InternetSA";
-const char* password = "cadebabaca";
-
-
+const int D4 = 2;
 
 int solenoidePin = 6;
 int drenagemPin = 7;
@@ -52,24 +32,14 @@ int bldcPin = 8;
 const int buttonPin = 0;
 int valorbutton = 0;
 
+//config time
 
-
-// Initialize Telegram BOT
-#define BOTtoken "5436108629:AAGxKKaYRn5JDtmyOLR8mhCDQeY59HgWQcc"  // your Bot Token (Get from Botfather)
-
-// Use @myidbot to find out the chat ID of an individual or a group
-// Also note that you need to click "start" on a bot before it can
-// message you
-#define CHAT_ID "-878096558"
-
-WiFiClientSecure secured_client;
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
-
-// Checks for new messages every 1 second.
-int botRequestDelay = 1000;
-unsigned long lastTimeBotRan;
-
+uint32_t targetTime = 0;                    // for next 1 second timeout
+static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
+uint8_t hh = conv2d(__TIME__), mm = conv2d(__TIME__ + 3), ss = conv2d(__TIME__ + 6); // Get H, M, S from compile time
+byte omm = 99, oss = 99;
+byte xcolon = 0, xsecs = 0;
+unsigned int colour = 0;
 
 // Inicia variáveis de tempo
 unsigned long millisTarefa1 = millis();
@@ -78,11 +48,6 @@ unsigned long millisTarefa3 = millis();
 unsigned long millisTarefa4 = millis();
 
 
-
-
-
-
-//Configura os voids
 
 
 void tarefa1(){
@@ -148,234 +113,240 @@ void tarefa1(){
 
 
 
-// Config do que acontece quando recebe msg
 
-void handleNewMessages(int numNewMessages) {
-  Serial.println("handleNewMessages");
-  Serial.println(String(numNewMessages));
-   
-  
 
-  for (int i=0; i<numNewMessages; i++) {
-    // Chat id of the requester
-    String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID){
-      bot.sendMessage(chat_id, "Unauthorized user", "");
-      continue;
-    }
-    
-    // Print the received message
-    String text = bot.messages[i].text;
-    Serial.println(text);
+void cuco(){
+if (targetTime < millis()) {
+    // Set next update for 1 second later
+    targetTime = millis() + 1000;
 
-    String from_name = bot.messages[i].from_name;
-
-    if (text == "/start") {
-      String welcome = "Bem vindo, " + from_name + ".\n";
-      welcome += "Use os comandos a seguir para controlar a Pandora .\n\n";
-      welcome += "/lavaroupa para ligar a maquina \n";
-      welcome += "/solenoide para ligar a maquina \n";
-      welcome += "/drenagem para ligar a maquina \n";
-      welcome += "/bldcfull para ligar a maquina \n";
-      welcome += "/lava_off to turn GPIO OFF \n";
-      welcome += "/state to request current GPIO state \n";
-      bot.sendMessage(chat_id, welcome, "");
-
-    }
-
-    if (text == "/lavaroupa") {
-      bot.sendMessage(chat_id, "Iniciando ciclo de lavagem", "");
-Serial.println("Iniciando ciclo");
-tarefa1();
-Serial.println("Ligando valvula solenoide");
-bot.sendMessage(chat_id, "liberando agua", "");
-delay(2000);
-tarefa3();
-Serial.println("Ligando motor bldc");
-delay(2000);
-tarefa2();
-Serial.println("Ligando drenagem");
-bot.sendMessage(chat_id,"ligando drenagem");
-delay(2000);
-tarefa4();
-tarefa2();
-Serial.println("Ligando motor fullspeed e drenagem");
-bot.sendMessage(chat_id,"ligando motor fullspeed e drenagem");
-delay(2000);
-Serial.println("Roupa limpinha");
-bot.sendMessage(chat_id,"Roupa limpinha");
-display.println("Roupa limpinha");
-delay(12000);
+    // Adjust the time values by adding 1 second
+    ss++;              // Advance second
+    if (ss == 60) {    // Check for roll-over
+      ss = 0;          // Reset seconds to zero
+      omm = mm;        // Save last minute time for display update
+      mm++;            // Advance minute
+      if (mm > 59) {   // Check for roll-over
+        mm = 0;
+        hh++;          // Advance hour
+        if (hh > 23) { // Check for 24hr roll-over (could roll-over on 13)
+          hh = 0;      // 0 for 24 hour clock, set to 1 for 12 hour clock
+        }
+      }
     }
 
 
- if (text == "/solenoide") {
-tarefa1();
-Serial.println("Ligando valvula solenoide");
-bot.sendMessage(chat_id, "liberando agua", "");
-delay(2000);
-}
-if (text == "/drenagem") {
-tarefa2();
-Serial.println("Ligando drenagem");
-bot.sendMessage(chat_id, "ligando drenagem", "");
-delay(2000);
-}
-if (text == "/bldcfull") {
-tarefa4();
-Serial.println("Ligando bldcfull");
-bot.sendMessage(chat_id, "ligando bldcfull", "");
-delay(12000);
-}
-
-
-
-
+    // Update digital time
+    int xpos = 0;
+    int ypos = 25; // Top left corner ot clock text, about half way down
+    int ysecs = ypos + 10;
+     tft.setTextSize(0);
 
     
-          if (text == "/lava_off") {
-      bot.sendMessage(chat_id, "Desligando todos os sensores", "");
-      Serial.println("Desligando sensores");
-      digitalWrite(drenagemPin, LOW);
-       digitalWrite(solenoidePin, LOW);
-        analogWrite(bldcPin, 0);
+
+    if (omm != mm) { // Redraw hours and minutes time every minute
+      omm = mm;
+      // Draw hours and minutes
+      if (hh < 10) xpos += tft.drawChar('0', xpos, ypos, 8); // Add hours leading zero for 24 hr clock
+      xpos += tft.drawNumber(hh, xpos, ypos, 8);             // Draw hours
+      xcolon = xpos; // Save colon coord for later to flash on/off later
+      xpos += tft.drawChar(':', xpos, ypos - 8, 8);
+      if (mm < 10) xpos += tft.drawChar('0', xpos, ypos, 8); // Add minutes leading zero
+      xpos += tft.drawNumber(mm, xpos, ypos, 8);             // Draw minutes
+      xsecs = xpos; // Sae seconds 'x' position for later display updates
     }
-    
-    if (text == "/state") {
-      if (digitalRead(solenoidePin)){
-        bot.sendMessage(chat_id, "Solenoide is ON", "");
+    if (oss != ss) { // Redraw seconds time every second
+      oss = ss;
+      xpos = xsecs;
+
+      if (ss % 2) { // Flash the colons on/off
+        tft.setTextColor(0x39C4, TFT_BLACK);        // Set colour to grey to dim colon
+        tft.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 6); // Seconds colon
+        tft.setTextColor(TFT_YELLOW, TFT_BLACK);    // Set colour back to yellow
       }
-      else{
-        bot.sendMessage(chat_id, "Solenoide is OFF", "");
+      else {
+        tft.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 6); // Seconds colon
       }
 
-      if (digitalRead(drenagemPin)){
-        bot.sendMessage(chat_id, "Drenagem is ON", "");
-      }
-      else{
-        bot.sendMessage(chat_id, "Drenagem is OFF", "");
-      }
-
-      if (digitalRead(bldcPin)){
-        bot.sendMessage(chat_id, "BLDC is ON", "");
-      }
-      else{
-        bot.sendMessage(chat_id, "BLDC is OFF", "");
-      }
+      //Draw seconds
+      if (ss < 10) xpos += tft.drawChar('0', xpos, ysecs, 6); // Add leading zero
+      tft.drawNumber(ss, xpos, ysecs, 6);                     // Draw seconds
     }
-  }
-}
+  }}
 
 
 
 
 
-
-
-void setup() {
-  Serial.begin(9600);
-
-  
-  
-
-  
-//inicia Pinos
-
-pinMode(drenagemPin, OUTPUT); //Define ldr (pino analógico A0) como saída
-pinMode(solenoidePin, OUTPUT); //Define ldr (pino analógico A0) como saída
-pinMode(bldcPin, OUTPUT); //Define ldr (pino analógico A0) como saída
-pinMode(buttonPin, INPUT); //Define ldr (pino analógico A0) como saída
-
-Serial.println();
-Serial.println();
-Serial.print("Connecting to ");
-Serial.println(ssid);
-WiFi.begin(ssid, password);
-secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-
-while (WiFi.status() != WL_CONNECTED)
+void setup()
 {
-delay(500);
-Serial.print(".");
-}
-Serial.println("");
-Serial.println("WiFi connected.");
-Serial.println("IP address: ");
-Serial.println(WiFi.localIP());
-timeClient.begin();
-display.begin(SSD1306_SWITCHCAPVCC);
-
-}
-
-void loop() {
-
-  
-//Mostra hora na tela
-timeClient.update();
-String formattedTime = timeClient.getFormattedTime();
-display.clearDisplay();
-display.setTextSize(3);                         // set these parameters according to your need..
-display.setCursor(0, 0);
-display.println(formattedTime);
-
-delay(2000); // Aguarda 2 segundos
-
-
-
-
-
-//verifica mensagem no telgram
- if (millis() > lastTimeBotRan + botRequestDelay)  {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-    while(numNewMessages) {
-      Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    }
-    lastTimeBotRan = millis();
-  }
-  
-  
-valorbutton = digitalRead(buttonPin);//Lê o valor do sensor ldr e armazena na variável valorldr
-Serial.println(valorbutton);//Imprime na serial os dados de valorldr
-
-
-if ((valorbutton) == LOW) { //Se o valor do button for LOW
-Serial.println("Iniciando ciclo");
-tarefa1();
-Serial.println("Ligando valvula solenoide");
-delay(2000);
-tarefa3();
-Serial.println("Ligando motor bldc");
-delay(2000);
-tarefa2();
-Serial.println("Ligando drenagem");
-bot.sendMessage(CHAT_ID,"ligando drenagem");
-delay(2000);
-tarefa4();
-tarefa2();
-Serial.println("Ligando motor fullspeed e drenagem");
-bot.sendMessage(CHAT_ID,"ligando motor fullspeed e drenagem");
-delay(2000);
-Serial.println("Roupa limpinha");
-bot.sendMessage(CHAT_ID,"Roupa limpinha");
-display.println("Roupa limpinha");
-  }
-
-  else { //Senão:
-
-   Serial.println("Fim de operacao");
-   delay(2000);
-   display.println(formattedTime);
-   delay(2000);
-   Serial.println(formattedTime);
-   delay(2000);
+   pinMode(D4, OUTPUT);//LED conectado à saida
+   WiFi.mode(WIFI_STA);//Define o WiFi como Estaçao
+   connect();//Funçao para Conectar ao WiFi
    
+//inicia tft
+ tft.init();
+    tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+
+Serial.begin(115200);
+  Serial.println();
+   // attempt to connect to Wifi network:
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
   }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(1000);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+
+   
+}
+
+void loop()
+{
+   if (millis() - tempo > 2000)//Faz a verificaçao das funçoes a cada 2 Segundos
+   {
+      connect();//Funçao para verificar se ainda há conexao
+      readTel();//Funçao para ler o telegram
+      tempo = millis();//Reseta o tempo
+   }
+
+   cuco();
+}
+
+
+void connect()//Funçao para Conectar ao wifi e verificar à conexao.
+{
+   if (WiFi.status() != WL_CONNECTED)//Caso nao esteja conectado ao WiFi, Ira conectarse
+   {
+      WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+      delay(2000);
+   }
+}
+
+
+void readTel()//Funçao que faz a leitura do Telegram.
+{
+   int newmsg = bot.getUpdates(bot.last_message_received + 1);
+
+   for (int i = 0; i < newmsg; i++)//Caso haja X mensagens novas, fara este loop X Vezes.
+   {
+      id = bot.messages[i].chat_id;//Armazenara o ID do Usuario à Váriavel.
+      text = bot.messages[i].text;//Armazenara o TEXTO do Usuario à Váriavel.
+      text.toUpperCase();//Converte a STRING_TEXT inteiramente em Maiuscúla.
+      from_name = bot.messages[i].from_name;
+      
+
+      if (text.indexOf("ON") > -1)//Caso o texto recebido contenha "ON"
+      {
+         digitalWrite(D4, 0);//Liga o LED
+         bot.sendMessage(id, "LED ON", "");//Envia uma Mensagem para a pessoa que enviou o Comando.
+         tft.setTextSize(3);
+         tft.fillScreen(TFT_BLUE);
+       
+
+Serial.println("Iniciando ciclo");
+Serial.println("Ligando valvula solenoide");
+bot.sendMessage(id, "liberando agua", "");
+       tft.drawString("ligando valvula", tft.width() / 2, tft.height() / 2);
+       tarefa1();
+ delay(5000);
+ 
+Serial.println("Ligando drenagem");
+bot.sendMessage(id,"ligando drenagem");
+       tft.drawString("drenando agua", tft.width() / 2, tft.height() / 2);
+       tarefa2();
+ delay(5000);
+
+ 
+Serial.println("Ligando motor  lavagem");
+bot.sendMessage(id,"ligando motor lavagem");
+       tft.drawString("lavando", tft.width() / 0, tft.height() / 0);
+       tarefa3();
+ delay(5000);
+ 
+Serial.println("Ligando motor fullspeed e drenagem");
+bot.sendMessage(id,"ligando motor fullspeed e drenagem");
+       tft.drawString("ligando motor", tft.width() / 2, tft.height() / 2);
+       tarefa2();
+       tarefa4();
+ delay(5000);
+ 
+Serial.println("Roupa limpinha");
+bot.sendMessage(id,"Roupa limpinha");
+tft.drawString("Roupa limpinha", tft.width() / 2, tft.height() / 2);
+
+         
+         delay(5000);
+         cuco();
+      }
+
+      else if (text.indexOf("OFF") > -1)//Caso o texto recebido contenha "OFF"
+      {
+         digitalWrite(D4, 1);//Desliga o LED
+         bot.sendMessage(id, "LED OFF", "");//Envia uma Mensagem para a pessoa que enviou o Comando.
+         tft.setTextSize(3);
+         tft.fillScreen(TFT_YELLOW);
+         tft.drawString("LED OFF", tft.width() / 2, tft.height() / 2);
+         delay(5000);
+         cuco();
+      }
+
+      else if (text.indexOf("START") > -1)//Caso o texto recebido contenha "START"
+      {
+         bot.sendSimpleMessage(id, id, "");//Envia uma mensagem com seu ID.
+      }
+      else if (text.indexOf("OLA") > -1)//Caso o texto recebido contenha "START"
+      {
+      if (from_name == "")
+      from_name = "Guest";
+
+      welcome = "Bem vindo, " + from_name + ".\n";
+      welcome += "Esse eh a PandoraBOT\n\n";
+      welcome += "/ledon : to switch the Led ON\n";
+      welcome += "/ledoff : to switch the Led OFF\n";
+      welcome += "/status : Returns current status of LED\n";
+      bot.sendMessage(id, welcome, "Markdown");
+      bot.sendSimpleMessage(id, id, "");//Envia uma mensagem com seu ID.
+      
+      }
+
+      else//Caso o texto recebido nao for nenhum dos acima, Envia uma mensagem de erro.
+      {
+    //     bot.sendSimpleMessage(id, "Comando Invalido", "");
+      }
+   }
+
+}
 
 
 
-
-
+// Function to extract numbers from compile time string
+static uint8_t conv2d(const char* p) {
+  uint8_t v = 0;
+  if ('0' <= *p && *p <= '9')
+    v = *p - '0';
+  return 10 * v + *++p - '0';
 }
